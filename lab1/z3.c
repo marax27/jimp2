d3a2c853365e3639cf4ea26533ae195681b6d7ef
marax27 <32137911+marax27@users.noreply.github.com>
2018-03-08 11:16:03 +0100

    Update z3.c

--- lab1/z3.c
+++ lab1/z3.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-	/*int n;
-	printf("Podaj ilosc liczb do wczytania: ");
-	if(scanf("%d", &n) != 1){
-		perror("Blad!");
-		return 1;
-	}
-
-	if(n <= 0){
-		perror("Niedodatnia ilosc.");
-		return 2;
-	}*/
-
 	double maximum, current;
 	int i = 1;
-	//for(int i=0; i!=n; ++i){
 	while(1){
 		printf("Liczba #%d: ", i++);
 		if(scanf("%lf", &current) != 1){
