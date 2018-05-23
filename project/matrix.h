af8fdb9e16e27ef7811e048b162551e783810d21
marax27 <kacpert314@gmail.com>
2018-05-23 21:15:22 +0200

    Matrix multiplication, assignment and more

--- project/matrix.h
+++ project/matrix.h
@@ -17,34 +17,69 @@ public:
 
 	~Matrix();
 
+	Matrix& operator=(const Matrix &model){
+		free();
+		copyElements(model);
+	}
+
 	// Not actual matrix multiplication; instead, multiples
 	// coresponding elements independently.
-	Matrix operator*(const Matrix &right) const;
+	Matrix operator*(const Matrix &right) const {
+		Matrix result = *this;
+		result *= right;
+		return result;
+	}
+
+	Matrix& operator*=(const Matrix &right);
+
+	// Multiply each element of a matrix by 'right'.
+	Matrix operator*(double right) const {
+		Matrix result = *this;
+		result *= right;
+		return result;
+	}
+
+	Matrix& operator*=(double right);
 
 	// Data access.
 
-	T& at(size_type row, size_type column){
+	inline T& at(size_type row, size_type column){
 		if(row >= _Rows || column >= _Columns)
 			throw std::out_of_range("Matrix::at: demanded element out of range.");
 		return operator()(row, column);
 	}
 
-	const T& at(size_type row, size_type column) const{
+	inline const T& at(size_type row, size_type column) const{
 		if(row >= _Rows || column >= _Columns)
 			throw std::out_of_range("Matrix::at: demanded element out of range.");
 		return operator()(row, column);
 	}
 
-	T& operator()(size_type row, size_type column){
+	inline T& operator()(size_type row, size_type column){
 		return data[row*_Columns + column];
 	}
 
-	const T& operator()(size_type row, size_type column) const{
+	inline const T& operator()(size_type row, size_type column) const{
 		return data[row*_Columns + column];
 	}
+
+	// Getters.
+
+	constexpr size_type size() const { return _Rows * _Columns; }
+	constexpr size_type rows() const { return _Rows; }
+	constexpr size_type columns() const { return _Columns; }
 
 private:
 	T *data;
+
+	inline void free(){
+		if(data){
+			delete[] data;
+			data = nullptr;
+		}
+	}
+
+	void copyElements(const Matrix &model);
 };
 
 //************************************************************
@@ -52,7 +87,7 @@ private:
 // Default constructor.
 template<typename T, uint32_t _Rows, uint32_t _Columns>
 Matrix<T,_Rows,_Columns>::Matrix(){
-	data = new T[_Rows * _Columns];
+	data = new T[size()];
 }
 
 // Move constructor.
@@ -62,17 +97,17 @@ Matrix<T,_Rows,_Columns>::Matrix(Matrix<T,_Rows,_Columns> &&model)
 
 // Copy constructor.
 template<typename T, uint32_t _Rows, uint32_t _Columns>
-Matrix<T,_Rows,_Columns>::Matrix(const Matrix<T,_Rows,_Columns> &model){
-	size_type size = _Rows * _Columns;
-	data = new T[size];
-	for(size_type i = 0; i != size; ++i)
-		data[i] = model.data[i];
+Matrix<T,_Rows,_Columns>::Matrix(
+	const Matrix<T,_Rows,_Columns> &model){
+
+	copyElements(model);
 }
 
 // An initializer_list-based constructor.
 template<typename T, uint32_t _Rows, uint32_t _Columns>
 Matrix<T,_Rows,_Columns>::Matrix(
 	std::initializer_list<std::initializer_list<T>> list){
+
 	// Check if numbers of rows and columns match.
 	if(list.size() != _Rows)
 		throw std::invalid_argument("Matrix::Matrix: invalid number of rows.");
@@ -82,7 +117,7 @@ Matrix<T,_Rows,_Columns>::Matrix(
 	}
 		
 	// Copy data from initializer_list.
-	data = new T[_Rows * _Columns];
+	data = new T[size()];
 	size_type idx = 0;
 	for(auto row : list){
 		for(auto elem : row)
@@ -95,12 +130,54 @@ Matrix<T,_Rows,_Columns>::Matrix(
 // Destructor.
 template<typename T, uint32_t _Rows, uint32_t _Columns>
 Matrix<T,_Rows,_Columns>::~Matrix(){
-	if(data){
-		delete[] data;
-		data = nullptr;
-	}
+	free();
 }
 
 //************************************************************
+
+template<typename T, uint32_t _Rows, uint32_t _Columns>
+Matrix<T,_Rows,_Columns>& Matrix<T,_Rows,_Columns>::operator*=(
+	const Matrix<T,_Rows,_Columns> &right) {
+	
+	for(size_type i = 0; i != size(); ++i)
+		data[i] *= right.data[i];
+	return *this;
+}
+
+//************************************************************
+
+template<typename T, uint32_t _Rows, uint32_t _Columns>
+Matrix<T,_Rows,_Columns>& Matrix<T,_Rows,_Columns>::operator*=(
+	double right) {
+	
+	for(size_type i = 0; i != size(); ++i)
+		data[i] *= right;
+	return *this;
+}
+
+//************************************************************
+
+template<typename T, uint32_t _Rows, uint32_t _Columns>
+void Matrix<T,_Rows,_Columns>::copyElements(
+	const Matrix<T,_Rows,_Columns> &model){
+
+	size_type sz = size();
+	data = new T[sz];
+	for(size_type i = 0; i != sz; ++i)
+		data[i] = model.data[i];
+}
+
+//************************************************************
+
+
+// double * Matrix
+template<typename T, uint32_t _Rows, uint32_t _Columns>
+Matrix<T,_Rows,_Columns> operator*(double left,
+	const Matrix<T,_Rows,_Columns> &right) {
+	
+	return right * left;
+}
+
+
 
 #endif  //_MATRIX_H_
